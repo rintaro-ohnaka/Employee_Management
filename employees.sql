e6721d06de11795927d5de6cf1d9200dbb75e2e9
rintaro <rintaro.onaka@d2c.co.jp>
2020-07-02 16:33:18 +0900

    Add: 社員情報一覧を表示する関数実装

--- employees.sql
+++ employees.sql
@@ -8,8 +8,13 @@ CREATE TABLE employee_table(
     employee_age INT,
     employee_gender VARCHAR(255),
     employee_image_id INT,
+    employee_postal_code INT,
+    employee_prefecture VARCHAR(255),
     employee_address VARCHAR(255),
-    employee_department_id INT,
+    department_id INT,
+    employee_start_date VARCHAR(255),
+    employee_leave_date VARCHAR(255),
+    employee_update_date VARCHAR(255),
     PRIMARY KEY (employee_id)
 )
 
@@ -18,13 +23,16 @@ CREATE TABLE employee_table(
 CREATE TABLE employee_image_table(
     employee_image_id INT AUTO_INCREMENT,
     employee_image VARCHAR(255),
+    employee_image_update_date VARCHAR(255),
     PRIMARY KEY (employee_image_id)
 )
 
 -- 部署テーブル
 
-CREATE TABLE employee_department_table(
-    employee_department_id INT AUTO_INCREMENT,
-    employee_department_name VARCHAR(255),
-    PRIMARY KEY (employee_department_id)
+CREATE TABLE department_table(
+    department_id INT AUTO_INCREMENT,
+    department_name VARCHAR(255),
+    department_create_date VARCHAR(255),
+    department_update_date VARCHAR(255),
+    PRIMARY KEY (department_id)
 )
